cc22b010f5e1d3a6353f4d0bda7831608654ef14
crd8 <146048274+crd8@users.noreply.github.com>
2023-12-08 10:13:00 +0700

    redesign ui

--- resources/views/department/trash.blade.php
+++ resources/views/department/trash.blade.php
@@ -139,11 +139,15 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
           </svg>
           <h3 class="mb-5 font-normal text-gray-500 dark:text-gray-400"><span class="font-bold uppercase text-yellow-500">Warning</span>: This action will restore archived data. Are you sure you want to restore the data with the name "<span class="font-bold underline text-gray-700 dark:text-gray-200">{{ $department->name }}</span>"?</h3>
-          <div class="inline-flex">
-            <a href="{{ route('department.restore', $department->id) }}" class="text-gray-700 dark:text-gray-300 hover:bg-green-700 dark:hover:bg-green-800 hover:text-white dark:hover:text-white focus:ring-4 focus:outline-none focus:ring-green-500 dark:focus:ring-green-800 rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2 mb-2">
-              Yes, I'm sure
-            </a>    
-            <button data-modal-hide="popup-modal-restore{{ $department->id }}" type="button" class="text-gray-700 dark:text-gray-300 bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-800 focus:outline-none dark:focus:ring-gray-800">No, cancel</button>
+          <div class="inline-flex w-full">
+            <div class="basis-1/2">
+              <a href="{{ route('department.restore', $department->id) }}" class="text-gray-700 w-full dark:text-gray-300 hover:bg-green-700 dark:hover:bg-green-800 hover:text-white dark:hover:text-white focus:ring-4 focus:outline-none focus:ring-green-500 dark:focus:ring-green-800 rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2 mb-2">
+                Yes, I'm sure
+              </a>    
+            </div>
+            <div class="basis-1/2">
+              <button data-modal-hide="popup-modal-restore{{ $department->id }}" type="button" class="text-gray-700 w-full dark:text-gray-300 bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-800 focus:outline-none dark:focus:ring-gray-800">No, cancel</button>
+            </div>
           </div>
         </div>
       </div>
