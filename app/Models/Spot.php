9ee5460693422d6df0898f0083f4582b8925c964
crd8 <146048274+crd8@users.noreply.github.com>
2023-12-29 15:09:52 +0700

    a

--- app/Models/Spot.php
+++ app/Models/Spot.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Spot extends Model
 {
     // use HasFactory;
+    use SoftDeletes;
     protected $dates = ['deleted_at'];
 
     protected $fillable = [
